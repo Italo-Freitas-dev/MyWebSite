1c79c1cddde9b317e562be2943d459b7caab47d7
MattFreitas-Dev <italomateusf1@gmail.com>
2025-07-08 16:33:46 -0300

    changes

--- dar_mode2.js
+++ dar_mode2.js
@@ -7,3 +7,13 @@ checkbox.addEventListener("change", () => {
     html.setAttribute("data-bs-theme", "light");
   }
 });
+
+const html2 = document.getElementById("outrosPage");
+const checkbox2 = document.getElementById("checkbox");
+checkbox2.addEventListener("change", () => {
+  if (checkbox2.checked) {
+    html2.setAttribute("data-bs-theme", "dark");
+  } else {
+    html2.setAttribute("data-bs-theme", "light");
+  }
+});
